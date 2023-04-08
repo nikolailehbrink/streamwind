91a22c7d5f4d3cc3e5a8f2a1be7e9e8aa1c6771b
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-04-08 23:47:07 +0200

    Update header.php
    - Make dark mode toggle bigger so it is the same size as the searchbar
    - Reduce gap between nav elements

--- header.php
+++ header.php
@@ -71,7 +71,7 @@
 					</div>
 				</div>
 
-				<div class="flex items-center gap-6">
+				<div class="flex items-center gap-4">
 					<?php
 					wp_nav_menu(
 						array(
@@ -86,8 +86,8 @@
 					);
 					?>
 
-					<button id="darkModeToggle" class="p-1 rounded-full bg-slate-100 dark:bg-slate-600 dark:text-white">
-						<svg xmlns="http://www.w3.org/2000/svg" class="dark:hidden h-7 w-7" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+					<button id="darkModeToggle" class="p-2 border-2 rounded-full bg-slate-100 dark:bg-slate-600 dark:text-white border-slate-200">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 dark:hidden" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 							<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 							<path d="M14.468 10a4 4 0 1 0 -5.466 5.46"></path>
 							<path d="M2 12h1"></path>
@@ -99,7 +99,7 @@
 							<path d="M15 13h5a2 2 0 1 0 0 -4"></path>
 							<path d="M12 16h5.714l.253 0a2 2 0 0 1 2.033 2a2 2 0 0 1 -2 2h-.286"></path>
 						</svg>
-						<svg xmlns="http://www.w3.org/2000/svg" class="hidden dark:block h-7 w-7" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						<svg xmlns="http://www.w3.org/2000/svg" class="hidden h-6 dark:block" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 							<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 							<path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
 							<path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2"></path>
